907e88a21f466ec678ff35099c73ea6f2ee1d254
tlugat <teo.lugat@ausha.co>
2023-10-27 23:34:15 +0200

    ✨ Appointment Calendar

---
+++ src/components/AppointmentTimeSlots/AppointmentTimeSlots.jsx
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types';
+import AppointmentTimeSlot from '@components/AppointmentTimeSlot';
+import { dayjs } from '@utils/dayjs';
+import { useAppointmentCalendar } from '@components/AppointmentCalendar';
+
+/* TODO: Get data from api */
+const TIME_SLOT_DURATION = 30;
+const OPENING_HOURS = {
+    open: dayjs().set('hour', 8).set('minute', 0),
+    close: dayjs().set('hour', 20).set('minute', 0),
+};
+
+const generateTimeSlots = () => {
+    const timeSlots = [];
+    let openingTime = OPENING_HOURS.open.clone();
+
+    while (openingTime.isSameOrBefore(OPENING_HOURS.close)) {
+        const timeSlot = openingTime.format('HH:mm');
+        timeSlots.push(timeSlot);
+        openingTime = openingTime.add(TIME_SLOT_DURATION, 'minutes');
+    }
+
+    return timeSlots;
+};
+
+const AppointmentTimeSlots = ({ date, ...props }) => {
+    const { onChange } = useAppointmentCalendar();
+    const timeSlots = generateTimeSlots();
+
+    const formatDateTime = (time) => {
+        const formattedDate = dayjs(`${date.year}-${date.month}-${date.day}`);
+        const formattedDateTime = formattedDate
+            .set('hour', time.split(':')[0])
+            .set('minute', time.split(':')[1]);
+        return formattedDateTime;
+    };
+
+    const handleSelectTimeSlot = (time) => {
+        const dateTime = formatDateTime(time);
+        onChange(dateTime);
+    };
+
+    return (
+        <div>
+            {timeSlots.map((timeSlot) => (
+                <AppointmentTimeSlot
+                    {...props}
+                    key={timeSlot}
+                    onPress={() => handleSelectTimeSlot(timeSlot)}
+                >
+                    {timeSlot}
+                </AppointmentTimeSlot>
+            ))}
+        </div>
+    );
+};
+
+AppointmentTimeSlots.propTypes = {
+    date: PropTypes.object.isRequired,
+};
+
+export default AppointmentTimeSlots;
