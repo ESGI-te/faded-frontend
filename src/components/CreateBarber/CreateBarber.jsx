399ad852644ec0b79f49a745224d17caa0f56c50
tlugat <teo.lugat@ausha.co>
2023-12-17 14:13:24 +0100

    ✨ Create Barber

---
+++ src/components/CreateBarber/CreateBarber.jsx
@@ -0,0 +1,44 @@
+import BarberForm from '@components/BarberForm';
+import useCreateBarberMutation from '@queries/barber/useCreateBarberMutation.hook';
+import { USER_ROLES } from '@utils/constants';
+import useUserQuery from '@queries/user/useUserQuery.hook';
+import PropTypes from 'prop-types';
+
+const CreateBarber = ({ onCloseModal }) => {
+    const { data: user } = useUserQuery();
+    const createBarber = useCreateBarberMutation();
+    const isProvider = user?.roles?.includes(USER_ROLES.PROVIDER);
+    const providerId = isProvider && user.provider.id;
+
+    const handleCreateBarber = (formData) => {
+        if (!isProvider || !providerId) return;
+        // Create new user with associated barber
+        const data = {
+            ...formData,
+            plainPassword: crypto.randomUUID(), // tmp password
+            barber: {
+                firstName: formData.firstName,
+                lastName: formData.lastName,
+                provider: providerId,
+            },
+            roles: [USER_ROLES.USER, USER_ROLES.BARBER],
+        };
+        createBarber.mutate(data, {
+            onSuccess: () => {
+                onCloseModal();
+            },
+        });
+    };
+
+    return <BarberForm onSubmit={handleCreateBarber} isLoading={createBarber.isLoading} />;
+};
+
+CreateBarber.propTypes = {
+    onCloseModal: PropTypes.func.isRequired,
+};
+
+CreateBarber.defaultProps = {
+    onCloseModal: () => {},
+};
+
+export default CreateBarber;
---
+++ src/components/CreateBarberModal/index.js
@@ -0,0 +1,2 @@
+export * from './CreateBarberModal';
+export { default } from './CreateBarberModal';
