ed95d8aa2222006dbda5ca75352e918679a5b7f5
tlugat <teo.lugat@ausha.co>
2024-02-12 15:00:17 +0100

    🐛 Fix appointment calendar

--- apps/website/src/components/AppointmentCalendar/useTimeSlots.hook.jsx
+++ apps/website/src/components/AppointmentCalendar/useTimeSlots.hook.jsx
@@ -1,14 +1,17 @@
 import useAppointmentsQuery from 'shared/src/queries/appointment/useAppointmentsQuery.hook';
 import useBarbersQuery from 'shared/src/queries/barber/useBarbersQuery.hook';
 import { dayjs } from '@utils/dayjs';
-import { isEmpty } from 'lodash';
 import { useParams } from 'react-router-dom';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(customParseFormat);
 
 const TIME_SLOT_DURATION = 30;
 
 const useTimeSlots = (date, planning, barberId) => {
     const { establishmentId } = useParams();
     const { data: appointments, isLoading } = useAppointmentsQuery({
+        pagination: false,
         establishment: establishmentId,
     });
     const { data: barbers } = useBarbersQuery({
@@ -16,23 +19,33 @@ const useTimeSlots = (date, planning, barberId) => {
         establishment: establishmentId,
     });
 
-    if (isEmpty(planning)) return [];
+    if (!planning || !planning.isOpen)
+        return {
+            timeSlots: [],
+            isLoading: false,
+            unavailableTimeSlots: [],
+        };
 
     const timeSlots = [];
 
     const fullOpeningDateTime = dayjs(date)
-        .set('hour', dayjs(planning.open).hour())
-        .set('minute', dayjs(planning.open).minute());
+        .set('hour', dayjs(planning.open, 'HH:mm').hour())
+        .set('minute', dayjs(planning.open, 'HH:mm').minute());
     const fullClosingDateTime = dayjs(date)
-        .set('hour', dayjs(planning.close).hour())
-        .set('minute', dayjs(planning.close).minute());
+        .set('hour', dayjs(planning.close, 'HH:mm').hour())
+        .set('minute', dayjs(planning.close, 'HH:mm').minute());
 
     let openingTime = fullOpeningDateTime;
     const closingTime = fullClosingDateTime;
     const unavailableTimeSlots = [];
+    const currentTime = dayjs();
 
     while (openingTime.isSameOrBefore(closingTime)) {
         const timeSlot = openingTime.format('HH:mm');
+
+        if (openingTime.isBefore(currentTime)) {
+            unavailableTimeSlots.push(timeSlot);
+        }
 
         const appointmentsForSlot = appointments?.filter((appointment) => {
             const appointmentStart = dayjs(appointment.dateTime);
@@ -41,7 +54,7 @@ const useTimeSlots = (date, planning, barberId) => {
             const isBooked =
                 openingTime.isSame(appointmentStart) ||
                 openingTime.isBetween(appointmentStart, appointmentEnd);
-
+            console.log(openingTime.isBefore(currentTime));
             if (barberId && isBooked && appointment.barber.id === barberId) {
                 unavailableTimeSlots.push(timeSlot);
             }
