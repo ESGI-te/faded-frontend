12a8ee9e223435b8f3e102715521666a0ac43fab
tlugat <teo.lugat@ausha.co>
2024-02-13 21:04:05 +0100

    🐛 Fix reset password

--- apps/website/src/components/ResetPassword/ResetPassword.jsx
+++ apps/website/src/components/ResetPassword/ResetPassword.jsx
@@ -1,7 +1,7 @@
+import { useEffect, useState } from 'react';
 import ResetPasswordForm from '@components/ResetPasswordForm';
 import useResetPasswordTokensQuery from 'shared/src/queries/resetPasswordToken/useResetPasswordTokensQuery.hook';
 import useUpdateUserPasswordMutation from 'shared/src/queries/user/useUpdateUserPasswordMutation.hook';
-
 import { useNavigate, useLocation } from 'react-router-dom';
 
 const ResetPassword = () => {
@@ -11,16 +11,18 @@ const ResetPassword = () => {
     const token = searchParams.get('token');
     const updateUserPassword = useUpdateUserPasswordMutation();
     const resetPasswordTokens = useResetPasswordTokensQuery(token);
+    const [data, setData] = useState();
 
-    const handleResetPassword = async ({ passwordConfirmation, ...data }) => {
-        if (!token) return;
-        await resetPasswordTokens.refetch({ throwOnError: true });
-
-        if (resetPasswordTokens.isError || !resetPasswordTokens.data) return;
+    useEffect(() => {
+        if (
+            resetPasswordTokens.isError ||
+            (!resetPasswordTokens.data && !resetPasswordTokens.isRefetching)
+        )
+            return;
 
         updateUserPassword.mutate(
             {
-                userId: resetPasswordTokens.data?.data?.[0]?.user.id,
+                userId: resetPasswordTokens.data.data?.[0]?.user.id,
                 user: data,
             },
             {
@@ -29,8 +31,18 @@ const ResetPassword = () => {
                 },
             },
         );
-    };
+    }, [
+        resetPasswordTokens.data,
+        resetPasswordTokens.isError,
+        resetPasswordTokens.isRefetching,
+        data,
+    ]);
 
+    const handleResetPassword = async ({ passwordConfirmation, ...formData }) => {
+        if (!token) return;
+        setData(formData);
+        await resetPasswordTokens.refetch({ throwOnError: true });
+    };
     return (
         <ResetPasswordForm
             onSubmit={handleResetPassword}
