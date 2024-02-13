2536501dcfef7659743236935d520e99751adc74
tlugat <teo.lugat@ausha.co>
2024-02-13 21:04:05 +0100

    🐛 Fix reset password

--- apps/manager/src/components/ResetPassword/ResetPassword.jsx
+++ apps/manager/src/components/ResetPassword/ResetPassword.jsx
@@ -1,7 +1,7 @@
+import { useEffect, useState } from 'react';
 import ResetPasswordForm from '@components/ResetPasswordForm';
 import useResetPasswordTokensQuery from 'shared/src/queries/resetPasswordToken/useResetPasswordTokensQuery.hook';
 import useUpdateUserPasswordMutation from 'shared/src/queries/user/useUpdateUserPasswordMutation.hook';
-
 import { useNavigate, useLocation } from 'react-router-dom';
 
 const ResetPassword = () => {
@@ -11,12 +11,14 @@ const ResetPassword = () => {
     const token = searchParams.get('token');
     const updateUserPassword = useUpdateUserPasswordMutation();
     const resetPasswordTokens = useResetPasswordTokensQuery(token);
+    const [data, setData] = useState();
 
-    const handleResetPassword = async ({ passwordConfirmation, ...data }) => {
-        if (!token) return;
-        await resetPasswordTokens.refetch({ throwOnError: true });
-
-        if (resetPasswordTokens.isError || !resetPasswordTokens.data) return;
+    useEffect(() => {
+        if (
+            resetPasswordTokens.isError ||
+            (!resetPasswordTokens.data && !resetPasswordTokens.isRefetching)
+        )
+            return;
 
         updateUserPassword.mutate(
             {
@@ -29,6 +31,17 @@ const ResetPassword = () => {
                 },
             },
         );
+    }, [
+        resetPasswordTokens.data,
+        resetPasswordTokens.isError,
+        resetPasswordTokens.isRefetching,
+        data,
+    ]);
+
+    const handleResetPassword = async ({ passwordConfirmation, ...formData }) => {
+        if (!token) return;
+        setData(formData);
+        await resetPasswordTokens.refetch({ throwOnError: true });
     };
 
     return (
