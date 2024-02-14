9c39121c65e152a6a8e2cbc1b8114f6b240fa1f5
kachakib <karmimchakib@gmail.com>
2024-02-15 00:27:34 +0100

    💄 add backgound and padding to category page, and remove settings from drawer

--- apps/admin/src/pages/CategoriesPage.jsx
+++ apps/admin/src/pages/CategoriesPage.jsx
@@ -1,17 +1,49 @@
 import ServiceCategory from '@components/ServiceCategory';
 import React from 'react';
 import styled from 'styled-components';
+import Text from 'shared/src/components/Text';
 
 function CategoriesPage() {
   return (
-    <Wrapper>
-      <ServiceCategory />
-    </Wrapper>
+      <Page>
+        <PageInnerWrapper>
+          <TitleWrapper>
+            <Text variant="headingM" fontWeight="--fw-bold">
+              Ajout de catégorie
+            </Text>
+            <Text variant="bodyL" color="--neutral500">
+              Gérez les catégories de services
+            </Text>
+          </TitleWrapper>
+          <ServiceCategory />
+        </PageInnerWrapper>
+      </Page>
   );
 }
 
-const Wrapper = styled.div`
-  padding: 2rem;
+const Page = styled.section`
+    display: flex;
+    align-items: start;
+    justify-content: center;
+    background-color: var(--background);
+`;
+const PageInnerWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 2rem;
+    width: 100%;
+    background-color: var(--background);
+    padding: var(--container-padding-mobile);
+    max-width: var(--container-width);
+
+    ${({ theme }) => theme.mediaQueries.desktopAndUp} {
+        padding: var(--container-padding);
+    }
+`;
+const TitleWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.25rem;
 `;
 
 export default CategoriesPage;
